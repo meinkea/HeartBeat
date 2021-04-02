919cff5455da2a33c4e0efcfc3f4bed1622ad423
Andrew E Meinke <meinkea@gmail.com>
2021-04-01 22:10:23 -0400

    Cleanup UKF.m
    Updated UKF.m's function calls with the new function names from previous commits and cleaned up comments and junk,

--- src/version/matlab/UKF.m
+++ src/version/matlab/UKF.m
@@ -1,4 +1,4 @@
-% v 1.0.2
+% v 1.0.4
 
 clear all
 close all
@@ -10,49 +10,53 @@ close all
 
 %==============================Plots=======================================
 
-Test_plots = 1; %flag
+Test_plots = 0; %flag
 
+%%
 %============================Initialize State==============================
-% x = [log2(80/Pref)  log2((500*c)/Rcref)  log2((0.00065/c)/Cref) log2((25000*c)/Rdref)...
-%      log2((500*c)/Rcref)+log2((25000*c)/Rdref)];
 
-%x = [[ 30.0];...
-%     [-10.0]];
-
-%P = [[32.0 15.0];...
-%     [15.0 40.0]];
-   
-   
+% State Vector
 x = [[ 30.0];...
      [-10.0];...
      [ 50.0];...
      [-10.0]];
 
+% State Estimator variance
 P = [[32.0 15.0  0.0  0.0];...
      [15.0 40.0  0.0  0.0];...
      [ 0.0  0.0 32.0 15.0];...
      [ 0.0  0.0 15.0 40.0]];
+
+% Process Model Variance and Covariance Matrix
+process_model_function = @xdot_func;
+
+Q = [[0.02  0.01  0.00  0.00];...
+     [0.01  0.02  0.00  0.00];...
+     [0.00  0.00  0.02  0.01];...
+     [0.00  0.00  0.01  0.02]];
+
+
+% Measurment Variance
+state_to_measurment_function = @h_func;
+
+R = [[0.3^2 0.0  ];...
+     [0.0   0.3^2]]; % For X meas only
+
 
 n = length(x);
 
 %number of measured Observations
 nmo = 2;
 
-%=========================Sigma Points tuning parameters===================
+% --- Sigma Points tuning parameters ---
 
 alpha = 0.3;
 beta = 2.0;
 kappa = -1; % prolly should be set to n-3, so -1
 
-%=========================Uncertainty and noise parameters=================
-% variance in noise from sensors
-%R = [[0.3^2 0.0  ];...
-%     [0.0   0.3^2]];
-   
-R = [[0.3^2 0.0  ];...
-     [0.0   0.3^2]]; % For X meas only
+  
 
-
+%%
 %===============Initial Guess and Parameters for DE solver=================
 % Measurement Time
 
@@ -78,10 +82,7 @@ rng('default')
 x0_init = [0.0 1.0, 0.0, 1.0]'; % True intial start
 meas_variance = 0.5;
 
-%[x_True, zm, tm] = Noisy_Measurements(@Pressure_Funtion, [dtm], x0_init, tm_start, dtm, tm_end, meas_variance);
-
-[x_True, zm_sol, tm] = Noisy_Measurements(@Pressure_Funtion, [dtm], x0_init, tm_start, dtm, tm_end, meas_variance);
-%[y_True, zm, tm] = Noisy_Measurements(@Pressure_Funtion, [dtm], x0_init, tm_start, dtm, tm_end, meas_variance);
+[x_True, zm_sol, tm] = noisy_measurements(process_model_function, [dtm], x0_init, tm_start, dtm, tm_end, meas_variance);
 
 zm = zm_sol(:,[1 3]); % For position meas only
 
@@ -108,7 +109,7 @@ t_prior = tm(1);       % time at prior, this is updated in loop
 dtp = 0.1;             % time per step in sigma predecition projection
 
 
-[Wc,Wm] =  computeWeights(n,lambda,alpha,beta); % this never changes, moved before loop
+[Wc,Wm] =  compute_weights(n,lambda,alpha,beta); % this never changes, moved before loop
 
 for i = 1:(length(tm)-1)
     % --- PREDICT ---
@@ -116,36 +117,35 @@ for i = 1:(length(tm)-1)
     
     t_prior = tm(i+1);
     %P
-    sigmaPoints = computeSigmaPoints(n, lambda, x, P); % Checked
+    sigmaPoints = compute_sigma_points(n, lambda, x, P); % Checked
     % dt = 0.002
     
     % Project sigma points using ODE solver
-    Sigmas_f = stateTransition(@Pressure_Funtion, [dtm], sigmaPoints, t_last_update, dtp, t_prior); % Checked
+    Sigmas_f = state_transition(process_model_function, [dtm], sigmaPoints, t_last_update, dtp, t_prior); % Checked
     
     % _bar denotes that it came from unscentedTransform
-    [ x_bar_xx, P_bar_xx ] =  unscentedTransform(Wm,Wc, Sigmas_f, Q); % Checked
     [ x_bar_xx, P_bar_xx ] =  unscented_transform(Wm,Wc, Sigmas_f, Q); % Checked
     
     % Recompute NEW sigmas at the new predicted state
-    Sigmas_x = computeSigmaPoints(n,lambda,x_bar_xx',P_bar_xx);
+    Sigmas_x = compute_sigma_points(n,lambda,x_bar_xx',P_bar_xx);
     
     % --- UPDATE ---
     
     % _h denotes measurement version of prediction
-    Sigmas_h = computeMeasurementSigmas_Z(@h_func, Sigmas_x, 0); 
+    Sigmas_h = state_to_measurment(state_to_measurment_function, Sigmas_x, 0); 
     
     % computeMeasurementMeanAndCovariance_Pz is just a redundant copy of unscentedTransform
-    [x_bar_hh, P_bar_hh] = unscentedTransform(Wm,Wc, Sigmas_h, R);
+    [x_bar_hh, P_bar_hh] = unscented_transform(Wm,Wc, Sigmas_h, R);
     
     P_bar_xh = cross_covariance(Wm, x_bar_xx, Sigmas_x, x_bar_hh, Sigmas_h);
     
-    K = compute_Kalman_gain(P_bar_xh, P_bar_hh);
+    K = compute_kalman_gain(P_bar_xh, P_bar_hh);
     
-    y = Residual_y(zm(i,:), x_bar_hh);
+    y = residual_y(zm(i,:), x_bar_hh);
     
-    P = CoverianceUpdate(K,P_bar_xx,P_bar_hh);
+    P = coveriance_update(K,P_bar_xx,P_bar_hh);
     
-    x = state_x(x_bar_xx,K,y); % wrong Sigmas where going in here!!!
+    x = state_update(x_bar_xx,K,y); % wrong Sigmas where going in here!!!
     
     
     sol = [sol,x];
