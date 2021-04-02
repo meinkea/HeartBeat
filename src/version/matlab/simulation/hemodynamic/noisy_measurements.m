e1b11a74d9cf56a783ffdec3729117e1af835fdb
Andrew E Meinke <meinkea@gmail.com>
2021-04-02 00:28:59 -0400

    made simulation\hemodynamic
    Made hemodynamic folder in simulation.
    Put a copy of noisy_measurements.m as temporary placeholders.

---
+++ src/version/matlab/simulation/hemodynamic/noisy_measurements.m
@@ -0,0 +1,17 @@
+% v 1.0.2
+function [y_true,zP,t] = noisy_measurements(func, Vargz, x, t_start, dt, t_end, StaD)
+%==============================================================
+% Adds Noise to the process model to simulate experimental
+% measurments
+%==============================================================
+  options = odeset('RelTol',1e-5,'Stats','on','OutputFcn',@odeplot);
+  
+  % Time
+  t = t_start:dt:t_end;
+  
+  % True Measurements
+  [t, y_true] = ode45(func, t, x, options, Vargz);
+  
+  % Pressure Measuments
+  zP = y_true + sqrt(StaD)*randn(size(y_true));
+end
