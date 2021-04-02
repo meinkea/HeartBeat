7972935d403735073a199e47dbb0d6540618f299
Andrew E Meinke <meinkea@gmail.com>
2021-04-02 05:12:34 -0400

    hot fix for pseudo state variable
    Hot fix to accept pseudo state inside state variable vectors, to preform parameter estimation.  This needs to be revised!

--- src/version/matlab/ukf/src/state_transition.m
+++ src/version/matlab/ukf/src/state_transition.m
@@ -5,7 +5,9 @@ function  Sigmas  = state_transition(func, Vargz, sigmaPoints, t_0, dt, t_1)
   
   Sigmas = zeros(size(sigmaPoints));
   parfor i = 1:size(sigmaPoints,1)
-    [~, res] = ode45(func, prediction_time_points, sigmaPoints(i,:)', options, Vargz);
+    % For 'pesudo states' for the parameter estimates
+    
+    [~, res] = ode45(func, prediction_time_points, sigmaPoints(i,:)', options, [sigmaPoints(i,2), sigmaPoints(i,3) sigmaPoints(i,4) Vargz(4) Vargz(5)]);
     Sigmas(i,:) = res(end,:)';
   end
   %Sigmas = sigmaPoints;
