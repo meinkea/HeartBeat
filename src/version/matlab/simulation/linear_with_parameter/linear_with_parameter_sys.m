e387732c584d031778be88db48558b28277eebff
Andrew E Meinke <meinkea@gmail.com>
2021-04-12 21:33:06 -0400

    relabeling vargz(1) as a
    Relabeled vargz(1) as a to show it is a state parameter.  No functional change made to this routine.

--- src/version/matlab/simulation/linear_with_parameter/linear_with_parameter_sys.m
+++ src/version/matlab/simulation/linear_with_parameter/linear_with_parameter_sys.m
@@ -1,6 +1,8 @@
 function res = linear_with_parameter_sys(t, y, vargz)
 
-  res(1) = -2*y(1) +2*y(2) +vargz(1);
+  a = vargz(1);
+  
+  res(1) = -2*y(1) +2*y(2) +a;
   res(2) = -4*y(1) +2*y(2);
   
   res = res';
