12f23128dca562402e5464d4239003f77b6c5ad2
Andrew E Meinke <meinkea@gmail.com>
2021-04-12 21:36:01 -0400

    added pseudo state observer
    The get_measurement function now generates fictitious estimates using a state observer

--- src/version/matlab/simulation/linear_with_parameter/linear_with_parameter__get_measurement.m
+++ src/version/matlab/simulation/linear_with_parameter/linear_with_parameter__get_measurement.m
@@ -1,10 +1,16 @@
-function res = linear_with_parameter__get_measurement(t, x, vargz)
+function res = linear_with_parameter__get_measurement(t, x, sol, vargz)
 
-  tm = vargz(1, :); % time series
-  zm = vargz([2 3], :); % measurement series
+  tm = vargz{1}; % time series
+  zm = vargz{2}; % measurement series
 
   index = find(tm ==t); % find index for measurment corresponding to time stamp
   
-  res = [zm(:, index)' 1.00];
+  if length(sol(1,:)) <= 1
+    pseudo_meas = vargz{3};
+  else
+    pseudo_meas = ( (sol(1,end)-sol(1,end-1)) / (tm(index)-tm(index-1)) )  - (-2*sol(1,end) +2*sol(2,end));
+  end
+  
+  res = [zm(:, index)' pseudo_meas];
   
 end
