bee216645a9662fe0c6697139fd774f210e5d0b3
Andrew E Meinke <meinkea@gmail.com>
2021-04-18 23:03:58 -0400

    corrected formula error
    Corrected an error in the formula.  R1 was expose to multiply dQ.
    Source is from https://en.m.wikipedia.org/wiki/Windkessel_effect, section "Three-element".
    
    Corrected names for P and P_init to dP and P, respectively.
    
    Refactored formula to be more readable.

--- src/version/matlab/simulation/hemodynamic/pressure_function.m
+++ src/version/matlab/simulation/hemodynamic/pressure_function.m
@@ -1,5 +1,5 @@
 %------------------------------------------------------------------
-function p = pressure_function(t,p_init, vargz)
+function dP = pressure_function(t,P, vargz)
   %==============================================================
   % Solves differential equation in the interval t with initial
   % guess y0
@@ -11,7 +11,7 @@ function p = pressure_function(t,p_init, vargz)
   cycle_time = vargz(4);
   Q_dt = vargz(5);
   
-  p = (1/C * ( blood_flow(t,cycle_time) + C*dQ(t,cycle_time,Q_dt) - (p_init / R2) + (blood_flow(t,cycle_time)*(R1/R2))));
+  dP = (1/C * ( (1+ (R1/R2))*blood_flow(t,cycle_time) + C*R1*dQ(t,cycle_time,Q_dt) - (P / R2))));
   
 end
 %------------------------------------------------------------------
